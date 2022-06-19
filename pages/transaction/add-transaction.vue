a912f6c244198a7292e22a1b1e20fea9ff43535a
Widya Ardianto <nartos9090@gmail.com>
2022-06-19 15:09:59 +0700

    add transaction (unused)

---
+++ pages/transaction/add-transaction.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <v-row>
+      <v-col cols="12">
+        <PageTitle title="Tambah Transaksi" />
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="12">
+        <v-chip-group>
+          <v-chip selected active-class="error">
+            Pengeluaran
+          </v-chip>
+
+          <v-chip active-class="success">
+            Pemasukan
+          </v-chip>
+        </v-chip-group>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import PageTitle from '@/components/PageTitle'
+export default {
+  name: 'add-transaction',
+
+  component: {
+    PageTitle
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
