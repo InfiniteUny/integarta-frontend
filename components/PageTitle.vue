9ab444c927d816f999b6070d6d8752089259dc39
Widya Ardianto <nartos9090@gmail.com>
2022-06-19 15:08:25 +0700

    page title component disable back button

---
+++ components/PageTitle.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="d-flex align-center">
+    <div v-if="!disableBack">
+      <v-btn text fab small class="mr-3" @click="$router.back()">
+        <v-icon>
+          mdi-chevron-left
+        </v-icon>
+      </v-btn>
+    </div>
+
+    <div class="font-weight-bold" style="font-size: 20px;">
+      {{ title }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageTitle',
+
+  props: {
+    title: {
+      type: String,
+      default: null
+    },
+    disableBack: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
